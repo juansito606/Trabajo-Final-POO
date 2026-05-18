90d961eceb8789c8ac092a78d02485790f403d0a
juansito606 <juanesizquierdo71@gmail.com>
2026-05-18 21:33:58 +0000

    error

--- Ascensor/PuertaAscensor.java
+++ Ascensor/PuertaAscensor.java
@@ -12,7 +12,14 @@ public class PuertaAscensor extends Puerta {
 
     }
     
-    pubic boolean isSensorActivo() {
+    public boolean isSensorActivo() {
         return sensorActivo;
+    }
+
+    public void setSensorActivo (boolean sensorActivo) {
+        this.sensorActivo = sensorActivo;
+        if (!sensorActivo) {
+            System.out.println("Alerta: Sensor desactivado.");
+        }
     }
 }
