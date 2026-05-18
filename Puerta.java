cb909af871cecc69d72801bf3c5aed217073659d
juansito606 <juanesizquierdo71@gmail.com>
2026-05-18 17:01:31 +0000

    correcion llaves

--- Puerta.java
+++ Puerta.java
@@ -3,35 +3,34 @@ public abstract class Puerta {
     private boolean abierta;
     private boolean hayObstaculo;
 
-    public Puerta () {
+    public Puerta() {
         this.abierta = false;
-        this.hayObstaculo = false;       
+        this.hayObstaculo = false;
     }
 
-    public void abrir () {
-        this.abierta =true;
-        System.out.println("Puerta abierta. ");
+    public void abrir() {
+        this.abierta = true;
+        System.out.println("Puerta abierta.");
     }
 
-    public void cerrar () {
+    public void cerrar() {
         if (hayObstaculo) {
-            System.out.println("Hay un obstaculo, no se puede cerrar");
+            System.out.println("Hay un obstaculo, no se puede cerrar.");
             return;
         }
-
         this.abierta = false;
-        System.out.println("Puerta cerrrada");
+        System.out.println("Puerta cerrada.");
+    }
 
-        public boolean detectarObstaculo () {
-            return hayObstaculo;
-        }
-        
-        public void setHayObstaculo (boolean hayObstaculo) {
-            this.hayObstaculo = hayObstaculo;
-        }
+    public boolean detectarObstaculo() {
+        return hayObstaculo;
+    }
 
-        public boolean isAbierta (){
-            return abierta;
-        }
+    public void setHayObstaculo(boolean hayObstaculo) {
+        this.hayObstaculo = hayObstaculo;
+    }
+
+    public boolean isAbierta() {
+        return abierta;
     }
 }
